14e9507eb16655e8e7ef123e0ecbc0be667d932a
EtienneBrJ <etiennebrxv@gmail.com>
2021-04-27 14:52:51 +0200

    rm 1 space

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,5 +10,4 @@ if last_dig > 5:
 elif last_dig == 0:
     print("Last digit of {} is {} and is 0".format(number, last_dig))
 else:
-    print("Last digit of {} is {} and is less\
- than 6 and not 0".format(number, last_dig))
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_dig))
