19df7095acac38669cf51874069acb9a1fbe1091
EtienneBrJ <etiennebrxv@gmail.com>
2021-05-18 20:10:23 +0000

    fix -?

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -46,9 +46,9 @@ class Square:
 
     def my_print(self):
         """ Print in stdout the current square with #"""
-
         if self.__size == 0:
             print('')
+            return
         for i in range(self.__position[1]):
             print('')
         for j in range(self.__size):
