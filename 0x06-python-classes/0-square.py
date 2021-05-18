018e831f9d91c513595cc011f77c077cf4f156a9
EtienneBrJ <etiennebrxv@gmail.com>
2021-05-18 14:58:30 +0000

    add 0/1

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """
+
+    Empty class that define s square
+
+    """
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """
+    Define a square
+    """
+    def __init__(self, size):
+        """ """
+        self._Square__size = size
