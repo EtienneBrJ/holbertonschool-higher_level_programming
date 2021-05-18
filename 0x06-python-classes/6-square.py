46800a95dbce9faa54963f58f4d9745c4e4a3ce1
EtienneBrJ <etiennebrxv@gmail.com>
2021-05-18 19:54:13 +0000

    fix -?

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -22,7 +22,7 @@ class Square:
         """Set the size to a value."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if self.__size < 0:
+        if self.size < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
