e2f36cadfaaa57d978ad7610c9bf06ec6a9a8bf2
EtienneBrJ <etiennebrxv@gmail.com>
2021-06-01 18:32:02 +0200

    fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,4 +8,4 @@ class MyList(list):
     def print_sorted(self):
         """ Public instance method
             Print an ascending sorted list."""
-        print(sorted(self[:]))
+        print(sorted(self))
