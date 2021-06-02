6d18e8c86cb929f006922a587abb770ce8601e93
EtienneBrJ <etiennebrxv@gmail.com>
2021-06-02 16:51:55 +0200

    Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -13,7 +13,17 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """ Retrieves a dictionary representation of a Student instance.
         """
-        return self.__dict__
+        dict = {}
+        if attrs is None:
+            return self.__dict__
+        
+        for i in attrs:
+            if isinstance(i, str) and hasattr(self, i):
+                dict[i] = getattr(self, i)
+            else:
+                pass
+
+        return dict
