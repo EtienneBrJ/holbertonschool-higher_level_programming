061ff8d2e0b981a3bdf15a624108a52d7da8974f
EtienneBrJ <etiennebrxv@gmail.com>
2021-06-02 17:34:16 +0200

    another way

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,18 +4,20 @@
 """
 
 
-from sys import argv
+import sys
 import json
+import os.path
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    list_elem = load_from_json_file("add_item.json")
-except:
-    list_elem = []
+list_elem = []
 
-for args in (argv[1:]):
-    list_elem.append(args)
+if os.path.exists('add_item.json') and os.path.getsize('add_item.json') > 0:
+    list_elem = load_from_json_file('add_item.json')
 
-save_to_json_file(list_elem, "add_item.json")
+if len(sys.argv) > 1:
+    for args in sys.argv[1:]:
+        list_elem.append(args)
+
+save_to_json_file(list_elem, 'add_item.json')
