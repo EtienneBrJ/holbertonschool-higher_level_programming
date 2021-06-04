bd097aba081ccc6576bad4ffe5080d224ad94334
EtienneBrJ <etiennebrxv@gmail.com>
2021-06-04 16:36:28 +0200

    Initial commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -48,4 +48,63 @@ class Rectangle(Base):
         """
         return self.__y
 
+    @width.setter
+    def width(self, value):
+        """ Sets the width.
+        """
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
+
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """ Sets the height.
+        """
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value <= 0:
+            raise ValueError('height must be > 0')
+
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        """ Sets the x.
+        """
+        if not isinstance(value, int):
+            raise TypeError('x must be an integer')
+        if value < 0:
+            raise ValueError('x must be >= 0')
+
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        """ Sets the y.
+        """
+        if not isinstance(value, int):
+            raise TypeError('y must be an integer')
+        if value < 0:
+            raise ValueError('y must be >= 0')
+
+        self.__y = value
+
+    def area(self):
+        """ Public method:
+            Returns the area value of the Rectangle instance
+        """
+        return self.__width * self.__height
+
+    def display(self):
+        """ Public method:
+            Prints in stdout the Rectangle instance with the character #
+        """
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print('#', end='')
+            print('')
+
     
