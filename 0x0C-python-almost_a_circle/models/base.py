590f2243fad5998a00c313712b04987a269dbbfb
EtienneBrJ <etiennebrxv@gmail.com>
2021-06-07 16:34:08 +0200

    Dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Module Base
 """
+import json
 
 
 class Base:
@@ -23,5 +24,11 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """ Static method:
+            Returns the JSON string representation
+            of list_dictionaries.
         """
-        """
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
