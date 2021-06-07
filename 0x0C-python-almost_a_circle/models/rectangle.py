28f092e440de61e48901353cb9faf6a201f7024e
EtienneBrJ <etiennebrxv@gmail.com>
2021-06-07 15:52:41 +0200

    add until tasks 15

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,7 +9,8 @@ class Rectangle(Base):
     """ Class inheriting from Base
     """
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ Private instance attributes (each with its own public getter and setter)
+        """ Private instance attributes
+            (each with its own public getter and setter)
             Args:
                 __width
                 __height
@@ -102,9 +103,69 @@ class Rectangle(Base):
         """ Public method:
             Prints in stdout the Rectangle instance with the character #
         """
+        for x in range(self.__y):
+            if self.__y > 1:
+                print('')
         for i in range(self.__height):
+            for y in range(self.__x):
+                print('', end=' ')
             for j in range(self.__width):
                 print('#', end='')
             print('')
 
-    
+    def __str__(self):
+        """ Return the print() and str() representation
+            of the Rectangle.
+        """
+        return '[Rectangle] ({}) {}/{} - {}/{}'.format(self.id, self.x, self.y,
+                                                       self.width, self.height)
+
+    def update(self, *args, **kwargs):
+        """ Public method:
+            assigns an argument to each attribute
+        """
+        if args and len(args) != 0:
+            i = 0
+            for arg in args:
+                if i == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif i == 1:
+                    self.width = arg
+                elif i == 2:
+                    self.height = arg
+                elif i == 3:
+                    self.x = arg
+                elif i == 4:
+                    self.y = arg
+
+                i += 1
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == 'id':
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == 'width':
+                    self.width = v
+                elif k == 'height':
+                    self.height = v
+                elif k == 'x':
+                    self.x = v
+                elif k == 'y':
+                    self.y = v
+
+    def to_dictionary(self):
+        """ Public method:
+            Returns the dictionary representation of a Rectangle
+        """
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+""" Module Square
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ Square class inherits from Rectangle
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Initialize an instance square from the class Rectangle.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ Return the print() and str() representation
+            of the Square.
+        """
+        return '[Rectangle] ({}) {}/{} - {}'.format(self.id, self.x, self.y,
+                                                    self.width)
+
+    @property
+    def size(self):
+        """ Retrieves the size of the square.
+        """
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        """ Sets the size of the Square.
+        """
+        self.width = size
+        self.height = size
+
+    def update(self, *args, **kwargs):
+        """ Public method:
+            assigns an argument to each attribute
+        """
+        if args and len(args) != 0:
+            i = 0
+            for arg in args:
+                if i == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
+                i += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == 'id':
+                    if v is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == 'size':
+                    self.width = v
+                elif k == 'x':
+                    self.x = v
+                elif k == 'y':
+                    self.y = v
+
+    def to_dictionary(self):
+        """ Public method:
+            Returns the dictionary representation of a Rectangle
+        """
+        return {
+            "id": self.id,
+            "size": self.size,
+            "x": self.x,
+            "y": self.y
+        }
