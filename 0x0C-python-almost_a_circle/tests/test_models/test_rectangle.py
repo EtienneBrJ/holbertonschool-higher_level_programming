6ee1ddbaf22dc444c283c2ca7d817451aad0eba2
EtienneBrJ <etiennebrxv@gmail.com>
2021-06-08 17:13:58 +0200

    add rectangle tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -71,6 +71,6 @@ class test_Rectangle (unittest.TestCase):
 
         self.assertEqual(7, Rectangle(10, 2, 0, 0, 7).id)
 
-        r6 = Rectangle(1, 2).area
+        r6 = Rectangle(1, 2).area()
         self.assertEqual(r6, 2)
 
