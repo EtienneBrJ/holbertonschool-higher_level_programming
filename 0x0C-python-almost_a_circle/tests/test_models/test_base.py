23f9edc7cfcb0f54d910875520db4b281e4b1ce2
EtienneBrJ <etiennebrxv@gmail.com>
2021-06-08 14:56:35 +0200

    add JSON tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -22,5 +22,28 @@ class test_Base (unittest.TestCase):
         b4 = Base(89)
         self.assertEqual(b4.id, 89)
 
-        b5 = Base()
-        self.assertEqual(b5.id, 4)
+    def test_to_json_string(self):
+        """Unittest for all the JSON manipulation"""
+        json_dictionary = Base.to_json_string(None)
+        self.assertEqual(json_dictionary, '[]')
+
+        json_dictionary2 = Base.to_json_string([])
+        self.assertEqual(json_dictionary, '[]')
+
+        json_dictionary3 = Base.to_json_string([{'id': 12}])
+        self.assertEqual(json_dictionary3, '[{"id": 12}]')
+        self.assertEqual(type(json_dictionary), str)
+    
+    def test_from_json_string(self):
+        """Unittest for all the JSON manipulation"""
+        json_dict = Base.from_json_string(None)
+        self.assertEqual(json_dict, [])
+
+        json_dict2 = Base.from_json_string("[]")
+        self.assertEqual(json_dict2, [])
+
+        json_dict3 = Base.from_json_string('[{ "id": 89 }]')
+        self.assertEqual(json_dict3, [{"id": 89}])
+        self.assertEqual(type(json_dict3), list)
+
+    
