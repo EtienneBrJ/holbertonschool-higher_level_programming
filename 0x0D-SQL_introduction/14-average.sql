ad7b77f9157b0468590f8f774a7cf393458e52ef
EtienneBrJ <etiennebrxv@gmail.com>
2021-07-12 20:32:52 +0200

    Average of a column with AVG()/Ch header col

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- Computes the score average of all records in the table second_table
 -- of the database passed in arguments
-SELECT AVG(`score`) FROM second_table;
+SELECT AVG(`score`) `average` FROM second_table;
