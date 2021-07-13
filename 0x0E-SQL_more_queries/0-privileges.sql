fcecd9f2e642ff5cf8d779983e41337a75f97e89
EtienneBrJ <etiennebrxv@gmail.com>
2021-07-13 12:21:57 +0200

    Initial commit

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- Lists all privileges of the MySQL user in localhost
 -- lists all granted privilege for user_0d_1 in a database
-SHOW GRANTS FOR user_0d_1@localhost;
--- lists all granted privilege for user_0d_2 in a database
-SHOW GRANTS FOR user_0d_2@localhost;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- lists all granted privilege for user_0d_ in a database
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
