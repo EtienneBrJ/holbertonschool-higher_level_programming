8b5295b8c0219c31e44af081b5df0c6c4a9fcb42
EtienneBrJ <etiennebrxv@gmail.com>
2021-07-13 12:23:14 +0200

    Initial commit

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,3 @@
 -- Create the table id_not_null
 -- if already exists, should not fail
-CREATE TABLE IF NOT EXISTS force_name (id INT DEFAULT 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
