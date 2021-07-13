b6e6cf53054d753e0f99a08fc0c12747149bf1ea
EtienneBrJ <etiennebrxv@gmail.com>
2021-07-13 14:39:17 +0200

    Add space between cmd and comments for the checker

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,9 +1,4 @@
 -- Lists all privileges of the MySQL user in localhost
 
--- lists all granted privilege for user_0d_1 in a database
-
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- lists all granted privilege for user_0d_ in a database
-
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
