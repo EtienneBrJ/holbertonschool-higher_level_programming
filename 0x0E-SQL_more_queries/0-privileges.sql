17192e34f69619f5f48d4e5e44afecc1a58c716e
EtienneBrJ <etiennebrxv@gmail.com>
2021-07-13 14:38:32 +0200

    Add space between cmd and comments for the checker

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -2,6 +2,7 @@
 -- lists all granted privilege for user_0d_1 in a database
 
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
 -- lists all granted privilege for user_0d_ in a database
 
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
