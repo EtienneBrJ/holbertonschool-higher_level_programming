a33f6e19407bc5a3a33a2ce1a6e597667db50400
EtienneBrJ <etiennebrxv@gmail.com>
2021-07-13 17:12:39 +0200

    Add tasks 10/11

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,6 +2,6 @@
 -- each record should display in ascending order tv_shows.title and tv_show_genres.genre_id
 -- only one SELECT
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows INNER JOIN tv_show_genres
+FROM tv_shows RIGHT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
