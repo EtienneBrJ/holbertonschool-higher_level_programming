b6b7a66dd5aba543a7c8c6ebc50771c95aeec518
EtienneBrJ <etiennebrxv@gmail.com>
2021-08-02 14:34:27 +0200

    Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-int1 = parseInt(process.argv[2])
-int2 = parseInt(process.argv[3])
+int1 = parseInt(process.argv[2]);
+int2 = parseInt(process.argv[3]);
 
-function add(a, b) {
-    console.log(a + b)
+function add (a, b) {
+  console.log(a + b);
 }
 
-add(int1, int2)
+add(int1, int2);
