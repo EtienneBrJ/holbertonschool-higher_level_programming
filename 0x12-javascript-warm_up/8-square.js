7ee34b7ee304ad0421e0f89532ccac21da83206b
EtienneBrJ <etiennebrxv@gmail.com>
2021-08-02 14:28:55 +0200

    fix semistandard

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ const mystring = parseInt(process.argv[2]);
 if (Number.isInteger(mystring) === Number.isInteger(false)) {
   console.log('Missing size');
 } else {
-    for (let i = 0; i < mystring; i++) {
-        console.log('X'.repeat(mystring));
-    }         
+  for (let i = 0; i < mystring; i++) {
+    console.log('X'.repeat(mystring));
+  }
 }
