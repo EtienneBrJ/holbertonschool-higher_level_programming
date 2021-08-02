92636cb0bb842bd504f60a0941d058feb26bfcfd
EtienneBrJ <etiennebrxv@gmail.com>
2021-08-02 14:53:52 +0200

    Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const int_arg1 = parseInt(process.argv[2]);
+
+function fact (num) {
+  if (num === 0) {
+    return 1;
+  } else {
+    return num * fact(num - 1);
+  }
+}
+
+if (isNaN(int_arg1)) {
+    console.log(1);
+} else {
+    console.log(fact(int_arg1));
+}
+
