adb76190ce18f2fd3901fa3e36f13fe452f771cb
EtienneBrJ <etiennebrxv@gmail.com>
2021-08-03 15:45:04 +0200

    Add Sorted Occurences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const d = require('./101-data').dict;
+const new_dict = {};
+
+for (const key in d) {
+    if (typeof (new_dict[d[key]]) === 'undefined') {
+        new_dict[d[key]] = [];
+    }
+    new_dict[d[key]].push(key);
+}
+console.log(new_dict);
