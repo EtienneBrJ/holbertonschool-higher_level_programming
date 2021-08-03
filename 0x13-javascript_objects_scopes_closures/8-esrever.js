ea7fe660d66cbbe38981be60389b1264609438c7
EtienneBrJ <etiennebrxv@gmail.com>
2021-08-03 03:46:46 +0200

    Reverse

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
 
-exports.esrever = function (list) {};
+exports.esrever = function (list) {
+    len = list.length;
+    for (let i = 0; i < len / 2; i++) {
+        [list[i], list[len - i - 1]] = [list[len - i - 1], list[i]];
+    }
+    return list;
+};
