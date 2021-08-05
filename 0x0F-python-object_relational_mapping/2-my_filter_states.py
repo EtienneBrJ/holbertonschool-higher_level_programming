cfbf44935514d03fc636211f3c4d53ccbac35ea9
EtienneBrJ <etiennebrxv@gmail.com>
2021-08-05 15:56:11 +0200

    Fix doc, pep8

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-""" Doc """
+""" Select all name from states starting matching with argv[4] """
 import sys
 import MySQLdb
 
-db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                     passwd=sys.argv[2], db=sys.argv[3])
-c = db.cursor()
-c.execute("SELECT * FROM states WHERE name LIKE '{}'".format(sys.argv[4]))
-[print(state) for state in c.fetchall()]
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                        passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
