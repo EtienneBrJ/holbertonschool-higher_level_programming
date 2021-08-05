19e67b5d613973b8ea583304d317e3c62755d3a3
EtienneBrJ <etiennebrxv@gmail.com>
2021-08-05 16:14:37 +0200

    Cities by states (JOIN)

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+""" Lists all cities from the database by states"""
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT cities.id, cities.name, states.name\
             FROM cities JOIN states\
