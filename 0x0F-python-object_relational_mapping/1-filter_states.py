fa73f3e19f174282736be7bc2516e825c6f5c54f
EtienneBrJ <etiennebrxv@gmail.com>
2021-08-05 15:52:21 +0200

    Init commit

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" Doc """
 import sys
 import MySQLdb
 
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+""" Doc """
 import sys
 import MySQLdb
-if __name__ == "__name__":
+
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                         passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
