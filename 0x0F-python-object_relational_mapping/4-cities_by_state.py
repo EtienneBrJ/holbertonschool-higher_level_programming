e982b224800c8c620e0f047356d840d1571e251e
EtienneBrJ <etiennebrxv@gmail.com>
2021-08-05 16:12:51 +0200

    Cities by states (JOIN)

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -3,7 +3,7 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, 
+    db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT cities.id, cities.name, states.name\
