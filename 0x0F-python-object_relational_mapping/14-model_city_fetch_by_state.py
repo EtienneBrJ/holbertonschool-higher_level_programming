696d91152bf55373b9866bfd0cfc4cc7082d793c
EtienneBrJ <etiennebrxv@gmail.com>
2021-08-06 12:50:40 +0200

    Fix

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -20,3 +20,5 @@ if __name__ == "__main__":
                         .filter(City.state_id == State.id)\
                         .order_by(City.id).all():
         print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    s.close()
