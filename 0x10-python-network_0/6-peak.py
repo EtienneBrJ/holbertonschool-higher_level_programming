296f84c87cdedadb6c4847c2a1c53982c1aa0000
EtienneBrJ <etiennebrxv@gmail.com>
2021-08-19 20:19:36 +0200

    Change peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-# Finds a peak in a list of unsorted integers
-
+"""Find a peak"""
 
 def find_peak(list_of_integers):
     """ Return the int_max of the list if not NULL"""
     if len(list_of_integers) == 0:
         return None
-    list_of_integers.sort()
-    return list_of_integers[-1]
+    return max(list_of_integers)
