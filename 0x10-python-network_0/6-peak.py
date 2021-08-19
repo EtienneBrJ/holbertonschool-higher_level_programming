b7e555414fd5a43b137e12c2a895e60784363936
EtienneBrJ <etiennebrxv@gmail.com>
2021-08-19 16:36:26 +0200

    Add white paper

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,6 +3,7 @@
 
 
 def find_peak(list_of_integers):
+    """ Return the int_max of the list if not NULL"""
     if len(list_of_integers) == 0:
         return None
     return max(list_of_integers)
