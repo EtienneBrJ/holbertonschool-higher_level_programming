890012eace0de7ad0fb3d0975dc30f81cc201f46
EtienneBrJ <etiennebrxv@gmail.com>
2021-08-21 06:59:13 +0200

    Add github API commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,13 +5,15 @@
 import requests
 from sys import argv
 
+
 if __name__ == "__main__":
     repo = argv[1]
     name = argv[2]
+    accept = {'Accept': 'application/vnd.github.v3+json'}
     response = requests.get(url="http://api.github.com/repos/{}/{}/commits"
-                                .format(name, repo), params={'per_page': 10},
-                                headers={'Accept': 'application/vnd.github.v3+json'})
+                            .format(name, repo), params={'per_page': 10},
+                            headers=accept)
     json = response.json()
     for line in json:
         print("{}: {}".format(line.get("sha"),
-                        line.get("commit").get("author").get("name")))
+              line.get("commit").get("author").get("name")))
