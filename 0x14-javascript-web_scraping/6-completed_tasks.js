a194aecca0bdbc0b631d0e4ebc9b3ccfd9649584
EtienneBrJ <etiennebrxv@gmail.com>
2021-09-14 17:32:23 +0200

    Dic

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,10 +1,25 @@
 #!/usr/bin/node
 const request = require('request');
 // const fs = require('fs');
-const apiUrl = process.argv[2]
+const apiUrl = process.argv[2];
 request.get(apiUrl, function (error, response, body) {
-      const content = JSON.parse(body)
-      if ()
-    });
+  if (error) {
+    console.error(error);
+  } else {
+    const content = JSON.parse(body);
+    const dic = {};
+    let i = 0;
 
-
+    for (i; i < content.length; i++) {
+      if (!(content[i].userId in dic)) {
+        if (content[i].completed === true) {
+          dic[content[i].userId] = 0;
+        }
+      }
+      if (content[i].completed === true) {
+        dic[content[i].userId] += 1;
+      }
+    }
+    console.log(dic);
+  }
+});
