e155c7a040a5027e65b54b4355f6b06a87237431
EtienneBrJ <etiennebrxv@gmail.com>
2021-09-14 15:43:19 +0200

    semistandard --fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,5 +3,5 @@ const request = require('request');
 
 const url = 'https://swapi-api.hbtn.io/api/films/';
 request(url.concat('', process.argv[2]), function (error, response, body) {
-    console.log(JSON.parse(body).title);
-  });
+  console.log(JSON.parse(body).title);
+});
--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,8 +2,8 @@
 const request = require('request');
 
 request(process.argv[2], function (error, response, body) {
-    const body_response = JSON.parse(body).results.filter((obj) => {
-        return obj.characters.filter((url) => { return url.endsWith('/18/'); }).length;
-      }).length;
-      console.log(body_response);
-    });
+  const body_response = JSON.parse(body).results.filter((obj) => {
+    return obj.characters.filter((url) => { return url.endsWith('/18/'); }).length;
+  }).length;
+  console.log(body_response);
+});
