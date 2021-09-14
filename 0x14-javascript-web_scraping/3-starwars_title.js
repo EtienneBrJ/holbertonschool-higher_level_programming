ea254a37c95657ec505d314f9bc48a3f6e0c93bf
EtienneBrJ <etiennebrxv@gmail.com>
2021-09-14 15:50:05 +0200

    Handle error/semistandard

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,5 +3,8 @@ const request = require('request');
 
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
   console.log(JSON.parse(body).title);
 });
--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,8 +2,11 @@
 const request = require('request');
 
 request(process.argv[2], function (error, response, body) {
-  const body_response = JSON.parse(body).results.filter((obj) => {
+  if (error) {
+    console.error(error);
+  }
+  const bodyResponse = JSON.parse(body).results.filter((obj) => {
     return obj.characters.filter((url) => { return url.endsWith('/18/'); }).length;
   }).length;
-  console.log(body_response);
+  console.log(bodyResponse);
 });
