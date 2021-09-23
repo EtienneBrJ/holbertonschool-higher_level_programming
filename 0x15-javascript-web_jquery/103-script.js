577217e9d6ef6cef61b18edbf9101bb935020cd0
EtienneBrJ <etiennebrxv@gmail.com>
2021-09-23 21:18:00 +0200

    Coucou

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,2 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() { 
-});
+document.addEventListener('DOMContentLoaded', function () {
+  $('#btn_translate').click(function () {
+    const code = $('#language_code').val();
+    $.getJSON('https://www.fourtonfish.com/hellosalut/?lang=' + code, function (data) {
+      $('#hello').html(data.hello);
+    });
+  });
